ad5057265bd4a51dfebb70ae8bfb8d754aa97fa3
Corentin DROUET <cdrouet@e3r8p1.42.fr>
2017-05-03 11:04:31 +0200

    update proto

--- libfts.h
+++ libfts.h
@@ -6,7 +6,7 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 08:35:19 by cdrouet           #+#    #+#             */
-/*   Updated: 2017/05/03 10:45:02 by cdrouet          ###   ########.fr       */
+/*   Updated: 2017/05/03 11:03:42 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,11 @@ extern int		ft_isascii(int c);
 extern int		ft_isprint(int c);
 extern int		ft_toupper(int c);
 extern int		ft_tolower(int c);
-extern int		ft_puts(char *c);
+extern int		ft_puts(const char *c);
 extern size_t	ft_strlen(const char *c);
 extern void		*ft_memset(void *b, int c, size_t len);
-extern void		*ft_memcpy(void *dst, void *src, size_t n);
+extern void		*ft_memcpy(void *restrict dst,
+					const void *restrict src, size_t n);
 extern char		*ft_strdup(const char *s1);
 extern void		ft_cat(int fd);
 extern void		ft_putnbr(int nb);
